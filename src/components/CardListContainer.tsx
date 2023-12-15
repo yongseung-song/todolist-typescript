1407b5d1d39ff4d97c2c2daaa5030f554a18357a
Yongseung Song <yongseung.song@icloud.com>
2023-12-15 16:06:36 +0900

    update: level 3 완료 -> db populate 깜빡했음

--- src/components/CardListContainer.tsx
+++ src/components/CardListContainer.tsx
@@ -1,13 +1,17 @@
 import { useEffect } from 'react';
+import { useAppSelector } from '../hooks/rtkHooks';
 import useTodos from '../hooks/useTodos';
+import { selectTodos } from '../redux/modules/todo.slice';
 import CardList from './CardList';
 
 function CardListContainer() {
   const { fetchTodos } = useTodos();
+  const todos = useAppSelector(selectTodos);
   useEffect(() => {
     fetchTodos();
   }, []);
 
+  console.log(todos);
   return (
     <div>
       <CardList isActive={true} />
